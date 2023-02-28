0c5d7d529ce117233ad0a1ae5f918e2ced072f63
to-my-learning-path <115326302+to-my-learning-path@users.noreply.github.com>
2023-02-27 21:07:05 -0500

    Refactored ResultSummary

--- src/components/ResultSummary/ResultSummary.tsx
+++ src/components/ResultSummary/ResultSummary.tsx
@@ -1,52 +1,54 @@
 import { useState } from "react";
 import Data from "../../../data.json";
 
+type ResultData = {
+  category: string;
+  score: number;
+  icon: string;
+};
+
 const ResultSummary = () => {
-  const [dataList, setDataList] = useState(Data);
+  const [dataList, setDataList] = useState<ResultData[]>(Data);
   return (
     <div>
-      <div>
-        <p>Your Result</p>
-        <p>76 of 100</p>
-        <p>Great</p>
-        <p>
-          You scored higher than 65% of the people who have taken these tests.
-        </p>
-      </div>
-      <div>
-        <p>Summary</p>
-        <ul>
-          {dataList.map((item, index) => (
-            <ScoreItem key={index} Item={item} />
-          ))}
-        </ul>
-        <button>Continue</button>
-      </div>
+      <ResultSection />
+      <SummarySection dataList={dataList} />
     </div>
   );
 };
 
-type ScoreItemProp = {
-  Item: {
-    category: string;
-    score: number;
-    icon: string;
-  };
-};
+const ResultSection = () => (
+  <div>
+    <p>Your Result</p>
+    <p>76 of 100</p>
+    <p>Great</p>
+    <p>You scored higher than 65% of the people who have taken these tests.</p>
+  </div>
+);
 
-const ScoreItem = ({ Item }: ScoreItemProp) => {
-  return (
-    <li>
-      <div>
-        <img src={getImageUrl(Item.icon)} alt="" />
-        <p>{Item.category}</p>
-      </div>
-      <p>
-        <span>{Item.score}</span> / 100
-      </p>
-    </li>
-  );
-};
+const SummarySection = ({ dataList }: { dataList: ResultData[] }) => (
+  <div>
+    <p>Summary</p>
+    <ul>
+      {dataList.map((item, index) => (
+        <ScoreItem key={index} Item={item} />
+      ))}
+    </ul>
+    <button>Continue</button>
+  </div>
+);
+
+const ScoreItem = ({ Item }: { Item: ResultData }) => (
+  <li>
+    <div>
+      <img src={getImageUrl(Item.icon)} alt="" />
+      <p>{Item.category}</p>
+    </div>
+    <p>
+      <span>{Item.score}</span> / 100
+    </p>
+  </li>
+);
 
 function getImageUrl(name: string) {
   return new URL(name, import.meta.url).href;
